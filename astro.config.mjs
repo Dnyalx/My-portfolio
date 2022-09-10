da17d62dfc6bce9b4e8ae9e94cb0c04a4172d159
Danny Alexander <rxn0088@hotmail.com>
2022-09-10 03:47:35 -0500

    Confg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  site: 'https://dnyalx.github.io',
-  base: '/my-portfolio',
-})
+  output: 'server',
+  adapter: netlify(),
+});
